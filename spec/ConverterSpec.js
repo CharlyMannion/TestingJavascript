37828ea54f722665846f5e0d183a7c1d5af4bf55
Charlotte Mannion <charlottemannion100@gmail.com>
2019-11-05 17:53:07 +0000

    writes test to convert celsius to fahrenheit

--- spec/ConverterSpec.js
+++ spec/ConverterSpec.js
@@ -6,5 +6,10 @@ describe("Cooking Ingredients Converter", function() {
 
       expect(result).toEqual('37.8');
     });
+
+    it("Celsius to Fahrenheit", function() {
+      var result = convertCelsiusToFrahrenheit(37.8);
+      expect(result).toEqual(100);
+    })
   });
 });
