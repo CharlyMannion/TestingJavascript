877692f46a4347343400a0e0c8f3627bf3c4ba93
Charlotte Mannion <charlottemannion100@gmail.com>
2019-11-05 18:00:54 +0000

    writes test for pund to kilo conversion

--- spec/ConverterSpec.js
+++ spec/ConverterSpec.js
@@ -3,13 +3,19 @@ describe("Cooking Ingredients Converter", function() {
   describe("Temperatures", function() {
     it("Fahrenheit to Celsius", function() {
       var result = convertFahrenheitToCelsius(100);
-
       expect(result).toEqual('37.8');
     });
 
     it("Celsius to Fahrenheit", function() {
       var result = convertCelsiusToFrahrenheit(37.8);
       expect(result).toEqual(100);
+    })
+  });
+
+  describe("Weights", function() {
+    it("Pounds to Kilos", function(){
+      var result = convertPoundsToKilos(10);
+      expect(result).toEqual(45.36);
     })
   });
 });
